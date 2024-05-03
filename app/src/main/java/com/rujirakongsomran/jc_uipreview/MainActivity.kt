310312c2cfbd5848ee5da1675e97753b119d06f1
Rujira Kongsomran <rujirabiwberry@gmail.com>
2024-05-03 18:38:25 +0700

    Dynamic color preview

--- app/src/main/java/com/rujirakongsomran/jc_uipreview/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_uipreview/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import com.rujirakongsomran.jc_uipreview.ui.theme.JC_UIPreviewTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,11 +32,30 @@ fun SimpleComposable() {
     Text(text = "Hello World")
 }
 
+@Composable
+fun DynamicColor() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Hello M3!")
+    }
+}
+
+@Preview(
+    showBackground = true,
+    wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE
+)
+@Composable
+fun DynamicColorPreview() {
+    JC_UIPreviewTheme {
+        DynamicColor()
+    }
+}
+
 @Preview
 @Composable
 fun SimpleComposablePreview() {
     SimpleComposable()
 }
+
 
 @Preview(widthDp = 50, heightDp = 50)
 @Composable
